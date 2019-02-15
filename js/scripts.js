feca52782ee4507faf07dadf557c1d9e75abf195
nyanxhtet <nyanhtet@yahoo.com>
2019-02-15 16:39:09 -0600

    created a repository array with 3 Pokemons created a for loop that write's each pokemon's name and height created a for loop that look for height over 1.0 and write to the dom it's name, height and "wow, that's big!)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-alert('Hello World');
+var repository = [
+
+  {name: 'bulbasaur',
+  height: .7,
+  type: 'grass'},
+
+  {name: 'charmander',
+  height: .6,
+  type: 'fire'},
+
+  {name: 'blastoise',
+  height: 1.6,
+  type: 'water'},
+];
+
+
+for (var i = 0; i <= 2; i ++){
+  document.write(repository[i].name+ " (Height:"+ repository[i].height+ ")  ");
+}
+
+for (var i = 0; i <= 2; i ++){
+  if (repository[i].height > 1){
+    document.write(repository[i].name+ " (Height:"+ repository[i].height+ ") -Wow, that's big!");
+  }
+}
