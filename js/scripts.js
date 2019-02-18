1ac88ed199abd8dd4b254a21d690795f53d3653e
nyanxhtet <nyanhtet@yahoo.com>
2019-02-18 00:14:56 -0600

    Cleaned up the for loop. Adjusted the Pokemon's type to arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,27 @@ var repository = [
 
   {name: 'bulbasaur',
   height: .7,
-  type: 'grass'},
+  type: ['grass','poison']},
 
   {name: 'charmander',
   height: .6,
-  type: 'fire'},
+  type: ['fire']},
 
   {name: 'blastoise',
   height: 1.6,
-  type: 'water'},
+  type: ['water']},
 ];
 
 
 for (var i = 0; i < repository.length; i ++){
-  document.write("<p>");
-  document.write(repository[i].name+ " (Height:"+ repository[i].height+ ")  ");
-  document.write("</p><br>");
 
-  if (repository[i].height > 1.0){
-    document.write("<p>");
-    document.write(repository[i].name+ " (Height:"+ repository[i].height+ ") -Wow, that's big!");
-    document.write("</p><br>");
+  document.write("<p>");
+  document.write(repository[i].name+ " (Height: "+ repository[i].height+ ") ")
+
+  if (repository[i].height > 1.0 ){
+    document.write("-Wow, that's big!");
   }
+
+  document.write("</p><br>");
 
 }
